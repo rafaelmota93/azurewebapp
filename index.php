70126b0bd3ca3ec06b93fa99b8e8ba78d5cce98a
Rafael Mota <87096745+rafaelmota93@users.noreply.github.com>
2023-04-12 12:41:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php>
 
-echo "<b>Aplicação do Rafael Mota rodando no Web Service App no Microsoft Azure</b>"
+echo "<b>Aplicação do Rafael Mota rodando no Web Service App no Microsoft Azure WebApp</b>"
